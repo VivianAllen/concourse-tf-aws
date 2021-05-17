5d89ee23206d0e0a0fe3b4ef0ca68cb55e9f3c97
Vivian Allen <vivian.allen@methods.co.uk>
2021-05-17 14:47:39 +0100

    remove ec2 tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -15,12 +15,3 @@ terraform {
 
 # NB all aws credentials got from concourse env - 'partial configuration'
 provider "aws" {}
-
-resource "aws_instance" "test_app_server" {
-  ami           = var.aws_ec2_ami_id
-  instance_type = "t2.micro"
-
-  tags = {
-    Name = var.aws_ec2_instance_name
-  }
-}
